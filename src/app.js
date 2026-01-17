4801b584e367acef3890e41517a5e9ad35e3e71a
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-01-17 15:27:25 -0300

    Adicionado Cors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
-import dotenv from 'dotenv';
-import {resolve} from 'path';
+import dotenv from "dotenv";
+import { resolve } from "path";
 dotenv.config();
 
-import './database/index.js';
+import "./database/index.js";
+import cors from "cors";
+import helmet from "helmet";
 
+import express from "express";
+import homeRoutes from "./routes/homeRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import tokenRoutes from "./routes/tokenRoutes.js";
+import alunosRoutes from "./routes/alunoRoutes.js";
+import fotoRoutes from "./routes/fotoRoutes.js";
 
-import express from 'express';
-import homeRoutes from './routes/homeRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import tokenRoutes from './routes/tokenRoutes.js';
-import alunosRoutes from './routes/alunoRoutes.js';
-import fotoRoutes from './routes/fotoRoutes.js';
+const whiteList = ["https://react2.34.72.34.75", "http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -20,17 +33,19 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true })); //Permite dados enviado por formularios HTML
     this.app.use(express.json()); //Permite que dados enviados em JSON sejam interpretados
-    this.app.use(express.static(resolve(__dirname, '..', 'uploads'))); //Define a pasta "uploads" como estatica para acesso externo
+    this.app.use(express.static(resolve(__dirname, "..", "uploads"))); //Define a pasta "uploads" como estatica para acesso externo
   }
 
   routes() {
-    this.app.use('/', homeRoutes);
-    this.app.use('/users', userRoutes);
-    this.app.use('/tokens', tokenRoutes);
-    this.app.use('/alunos', alunosRoutes);
-     this.app.use('/fotos', fotoRoutes);
+    this.app.use("/", homeRoutes);
+    this.app.use("/users", userRoutes);
+    this.app.use("/tokens", tokenRoutes);
+    this.app.use("/alunos", alunosRoutes);
+    this.app.use("/fotos", fotoRoutes);
   }
 }
 
--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -1,31 +1,33 @@
-import User from '../models/User.js';
-import jwt from 'jsonwebtoken';
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
-class TokenController { //definindo a classe TokenController
-    async store(req, res) {
-      const {email = '', password = ''} = req.body; //pegando email e senha do corpo da requisição
+class TokenController {
+  //definindo a classe TokenController
+  async store(req, res) {
+    const { email = "", password = "" } = req.body; //pegando email e senha do corpo da requisição
 
-      if(!email || !password) {
-        return res.status(401).json({ errors: ['Credenciais invalidas']})
-      }
-
-      const user = await User.findOne({ where: { email } }); //procurando o usuario pelo email - findoneone retorna o primeiro que encontrar
-
-      if(!user) {
-        return res.status(401).json({ errors: ['Usuario nao existe']})
-      }
-
-      if(!(await user.passwordisValid(password))) { //verificando se a senha é válida q setamos no user.js
-        return res.status(401).json({ errors: ['Senha invalida']})
-      }
-
-      const {id} = user; //pegando o id do usuario
-      const token = jwt.sign({ id, email}, process.env.TOKEN_SECRET, {
-        expiresIn: process.env.TOKEN_EXPIRATION,
-      }); //gerando o token JWT- sign recebe 3 parametros: 1º o payload (dados que queremos colocar no token), 2º a chave secreta para assinar o token, 3º opções do token (tempo de expiração)
-
-      return res.json({token}); //retornando o token
+    if (!email || !password) {
+      return res.status(401).json({ errors: ["Credenciais invalidas"] });
     }
+
+    const user = await User.findOne({ where: { email } }); //procurando o usuario pelo email - findoneone retorna o primeiro que encontrar
+
+    if (!user) {
+      return res.status(401).json({ errors: ["Usuario nao existe"] });
+    }
+
+    if (!(await user.passwordisValid(password))) {
+      //verificando se a senha é válida q setamos no user.js
+      return res.status(401).json({ errors: ["Senha invalida"] });
+    }
+
+    const { id } = user; //pegando o id do usuario
+    const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    }); //gerando o token JWT- sign recebe 3 parametros: 1º o payload (dados que queremos colocar no token), 2º a chave secreta para assinar o token, 3º opções do token (tempo de expiração)
+
+    return res.json({ token, user: { nome: user.nome, id, email } }); //retornando o token
   }
+}
 
 export default new TokenController();
