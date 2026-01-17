f58647646af1d17db28070d1b3fe805df527d06c
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-01-17 15:30:22 -0300

    Adicionando Cors

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,8 @@ var _path = require('path');
 _dotenv2.default.config();
 
 require('./database/index.js');
-
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _homeRoutesjs = require('./routes/homeRoutes.js'); var _homeRoutesjs2 = _interopRequireDefault(_homeRoutesjs);
@@ -11,6 +12,18 @@ var _userRoutesjs = require('./routes/userRoutes.js'); var _userRoutesjs2 = _int
 var _tokenRoutesjs = require('./routes/tokenRoutes.js'); var _tokenRoutesjs2 = _interopRequireDefault(_tokenRoutesjs);
 var _alunoRoutesjs = require('./routes/alunoRoutes.js'); var _alunoRoutesjs2 = _interopRequireDefault(_alunoRoutesjs);
 var _fotoRoutesjs = require('./routes/fotoRoutes.js'); var _fotoRoutesjs2 = _interopRequireDefault(_fotoRoutesjs);
+
+const whiteList = ["https://react2.34.72.34.75", "http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -20,17 +33,19 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true })); //Permite dados enviado por formularios HTML
     this.app.use(_express2.default.json()); //Permite que dados enviados em JSON sejam interpretados
-    this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads'))); //Define a pasta "uploads" como estatica para acesso externo
+    this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, "..", "uploads"))); //Define a pasta "uploads" como estatica para acesso externo
   }
 
   routes() {
-    this.app.use('/', _homeRoutesjs2.default);
-    this.app.use('/users', _userRoutesjs2.default);
-    this.app.use('/tokens', _tokenRoutesjs2.default);
-    this.app.use('/alunos', _alunoRoutesjs2.default);
-     this.app.use('/fotos', _fotoRoutesjs2.default);
+    this.app.use("/", _homeRoutesjs2.default);
+    this.app.use("/users", _userRoutesjs2.default);
+    this.app.use("/tokens", _tokenRoutesjs2.default);
+    this.app.use("/alunos", _alunoRoutesjs2.default);
+    this.app.use("/fotos", _fotoRoutesjs2.default);
   }
 }
 
--- dist/controllers/TokenController.js
+++ dist/controllers/TokenController.js
@@ -1,31 +1,33 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _Userjs = require('../models/User.js'); var _Userjs2 = _interopRequireDefault(_Userjs);
 var _jsonwebtoken = require('jsonwebtoken'); var _jsonwebtoken2 = _interopRequireDefault(_jsonwebtoken);
 
-class TokenController { //definindo a classe TokenController
-    async store(req, res) {
-      const {email = '', password = ''} = req.body; //pegando email e senha do corpo da requisição
+class TokenController {
+  //definindo a classe TokenController
+  async store(req, res) {
+    const { email = "", password = "" } = req.body; //pegando email e senha do corpo da requisição
 
-      if(!email || !password) {
-        return res.status(401).json({ errors: ['Credenciais invalidas']})
-      }
-
-      const user = await _Userjs2.default.findOne({ where: { email } }); //procurando o usuario pelo email - findoneone retorna o primeiro que encontrar
-
-      if(!user) {
-        return res.status(401).json({ errors: ['Usuario nao existe']})
-      }
-
-      if(!(await user.passwordisValid(password))) { //verificando se a senha é válida q setamos no user.js
-        return res.status(401).json({ errors: ['Senha invalida']})
-      }
-
-      const {id} = user; //pegando o id do usuario
-      const token = _jsonwebtoken2.default.sign({ id, email}, process.env.TOKEN_SECRET, {
-        expiresIn: process.env.TOKEN_EXPIRATION,
-      }); //gerando o token JWT- sign recebe 3 parametros: 1º o payload (dados que queremos colocar no token), 2º a chave secreta para assinar o token, 3º opções do token (tempo de expiração)
-
-      return res.json({token}); //retornando o token
+    if (!email || !password) {
+      return res.status(401).json({ errors: ["Credenciais invalidas"] });
     }
+
+    const user = await _Userjs2.default.findOne({ where: { email } }); //procurando o usuario pelo email - findoneone retorna o primeiro que encontrar
+
+    if (!user) {
+      return res.status(401).json({ errors: ["Usuario nao existe"] });
+    }
+
+    if (!(await user.passwordisValid(password))) {
+      //verificando se a senha é válida q setamos no user.js
+      return res.status(401).json({ errors: ["Senha invalida"] });
+    }
+
+    const { id } = user; //pegando o id do usuario
+    const token = _jsonwebtoken2.default.sign({ id, email }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    }); //gerando o token JWT- sign recebe 3 parametros: 1º o payload (dados que queremos colocar no token), 2º a chave secreta para assinar o token, 3º opções do token (tempo de expiração)
+
+    return res.json({ token, user: { nome: user.nome, id, email } }); //retornando o token
   }
+}
 
 exports. default = new TokenController();
